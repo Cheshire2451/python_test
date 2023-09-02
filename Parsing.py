e9cd6e3210eee1515f092a32b4bd2a1f582714d6
Max <Cheshir2451@gmail.com>
2023-09-02 19:56:18 +0300

    Тест парсинга

--- Parsing.py
+++ Parsing.py
@@ -1,18 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+for count in range(1,8):
+    url = f'https://scrapingclub.com/exercise/list_basic/?page={count}'
 
-url = 'https://scrapingclub.com/exercise/list_basic/?page=1'
+    response = requests.get(url)
 
-response = requests.get(url)
+    soup = BeautifulSoup(response.text, "lxml")
 
-soup = BeautifulSoup(response.text, "lxml")
-
-data = soup.find_all("div", class_="w-full rounded border")
+    data = soup.find_all("div", class_="w-full rounded border")
 
 
-for i in data:
-    price = i.find('h5').text
-    name = i.find('h4').text
-    url_img = "https://scrapingclub.com" + i.find("img", class_="card-img-top img-fluid").get("src")
-    print(name +"\n" + price + "\n" + url_img)
+    for i in data:
+        price = i.find('h5').text
+        name = i.find('h4').text
+        url_img = "https://scrapingclub.com" + i.find("img", class_="card-img-top img-fluid").get("src")
+        print(name +"\n" + price + "\n" + url_img)
 
